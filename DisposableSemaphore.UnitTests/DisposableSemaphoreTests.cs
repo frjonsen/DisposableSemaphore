49339356f46515518c49df8dc043c83e2ba686e4
Fredrik Jonsén <fredrikjonsen@gmail.com>
2017-09-02 15:36:41 +0200

    Implementation of 0.1 done, with tests

--- DisposableSemaphore.UnitTests/DisposableSemaphoreTests.cs
+++ DisposableSemaphore.UnitTests/DisposableSemaphoreTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace DisposableSemaphore.UnitTests
@@ -6,9 +8,45 @@ namespace DisposableSemaphore.UnitTests
     public class DisposableSemaphoreTests
     {
         [Fact]
-        public void SimpleTest()
+        public async Task DisposingTest()
         {
-            Assert.Equal(3, DisposableSemaphore.SomethingTestable);
+            var semaphore = new DisposableSemaphore(1, 1);
+
+            using (await semaphore.WaitAsync())
+            {
+                Assert.Equal(semaphore.CurrentCount, 0);
+            }
+
+            Assert.Equal(semaphore.CurrentCount, 1);
+        }
+
+        [Fact]
+        public async Task ThreadingTest()
+        {
+            var semaphore = new DisposableSemaphore(1, 1);
+            var shared = 0;
+
+            async Task TestFunc(int val)
+            {
+                using (await semaphore.WaitAsync())
+                {
+                    shared = val;
+                }
+            }
+
+            var t1 = Task.Run(() => TestFunc(3));
+            await t1;
+            Assert.Equal(3, shared);
+
+            var t2 = Task.Run(() => TestFunc(5));
+            await t2;
+            Assert.Equal(5, shared);
+
+            var t3 = Task.Run(() => TestFunc(3));
+            await t3;
+            Assert.Equal(3, shared);
+
+
         }
     }
 }
--- DisposableSemaphore/DisposableSemaphore.cs
+++ DisposableSemaphore/DisposableSemaphore.cs
@@ -1,9 +1,45 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DisposableSemaphore
 {
     public class DisposableSemaphore
     {
-        public const int SomethingTestable = 3;
+        private readonly SemaphoreSlim _semaphore;
+
+        public int CurrentCount => this._semaphore.CurrentCount;
+
+        public DisposableSemaphore(int initialCount, int maxCount)
+        {
+            this._semaphore = new SemaphoreSlim(initialCount, maxCount);
+        }
+
+        public async Task<DisposableSemaphoreHandle> WaitAsync()
+        {
+            var handle = new DisposableSemaphoreHandle(this._semaphore);
+            await handle.WaitAsync();
+            return handle;
+        }
+
+        public class DisposableSemaphoreHandle : IDisposable
+        {
+            private readonly SemaphoreSlim _semaphore;
+
+            internal DisposableSemaphoreHandle(SemaphoreSlim instance)
+            {
+                this._semaphore = instance;
+            }
+
+            public void Dispose()
+            {
+                this._semaphore.Release();
+            }
+
+            internal async Task WaitAsync()
+            {
+                await this._semaphore.WaitAsync();
+            }
+        }
     }
 }
